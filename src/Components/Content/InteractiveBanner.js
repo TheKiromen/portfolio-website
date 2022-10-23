4e56243c53a8267cbf0bf3d9b1c792b511f1c5fb
Dominik Kruczek <TheKiromen@gmail.com>
2022-10-23 20:24:53 +0200

    Integrated images
    Integrated images to be imported from Assets folder and displayed. Changed canvas to draw contents after image is loaded.

--- src/Components/Content/InteractiveBanner.js
+++ src/Components/Content/InteractiveBanner.js
@@ -1,12 +1,14 @@
+import Banner from "../../Assets/Images/banner.png";
 
 function InteractiveBanner(){
     //Object references
     let navbar;
     let canvas;
     let ctx;
+    let image = new Image();
 
     //Helper variables
-    let offset,width,height;
+    let offset,width,height, imageWidth, imageHeight;
 
     const handleResize = () => {
         //Get website dimensions and canvas offset
@@ -24,11 +26,14 @@ function InteractiveBanner(){
         //Move origin to the middle
         ctx.translate(width/2,offset+135);
 
-        //TODO draw the image
+        //Draw the image
+        //TODO create the particle simulation from the image and draw it here
+        ctx.drawImage(image,-imageWidth/2,-imageHeight/2);
 
-        ctx.beginPath();
-        ctx.arc(0, 0, 100, 0, 2 * Math.PI);
-        ctx.stroke();
+        // ctx.beginPath();
+        // ctx.arc(0, 0, 10, 0, 2 * Math.PI);
+        // ctx.stroke();
+
     }
 
     //Get canvas context on page load
@@ -38,18 +43,27 @@ function InteractiveBanner(){
         navbar=document.getElementById("menu");
         ctx=canvas.getContext('2d');
 
+        //Setup banner image
+        //TODO make the banner image bigger?
+        image.src=Banner;
+
         //Set canvas position
         canvas.style.width='100%';
         canvas.style.position='absolute';
         canvas.style.top="0";
         canvas.style.zIndex="-1";
+    });
 
-        //Redraw the canvas
+    //Draw onto canvas only once the image is loaded
+    image.addEventListener("load",()=>{
+        imageWidth=image.width;
+        imageHeight=image.height;
         handleResize();
     });
 
     //Redraw the canvas on window resize
     window.addEventListener("resize",handleResize);
+
 
     return(
         <canvas/>
